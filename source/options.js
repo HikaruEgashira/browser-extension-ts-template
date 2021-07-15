80006440368b74dde258daa628d819409e827507
HikaruEgashira <s1811320@gmail.com>
2021-07-15 14:07:03 +0900

    ts化

--- source/options.js
+++ source/options.js
@@ -1,20 +1,29 @@
 // Don't forget to import this wherever you use it
-import browser from 'webextension-polyfill';
+// @ts-ignore
+import browser from 'webextension-polyfill-ts';
 
 import optionsStorage from './options-storage.js';
 
 optionsStorage.syncForm('#options-form');
 
-const rangeInputs = [...document.querySelectorAll('input[type="range"][name^="color"]')];
-const numberInputs = [...document.querySelectorAll('input[type="number"][name^="color"]')];
+const rangeInputs = [
+	// @ts-ignore
+	...document.querySelectorAll('input[type="range"][name^="color"]')
+];
+const numberInputs = [
+	// @ts-ignore
+	...document.querySelectorAll('input[type="number"][name^="color"]')
+];
 const output = document.querySelector('.color-output');
 
 function updateColor() {
+	// @ts-ignore
 	output.style.backgroundColor = `rgb(${rangeInputs[0].value}, ${rangeInputs[1].value}, ${rangeInputs[2].value})`;
 }
 
 function updateInputField(event) {
-	numberInputs[rangeInputs.indexOf(event.currentTarget)].value = event.currentTarget.value;
+	numberInputs[rangeInputs.indexOf(event.currentTarget)].value =
+		event.currentTarget.value;
 }
 
 for (const input of rangeInputs) {
